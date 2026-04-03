36cfeedbc0fb99681c11bd56c04b9f6f7f28b3ca
Daniel Muñoz Mendoza <munozmendozadaniel4@gmail.com>
2026-04-02 20:17:00 -0600

    corregido

--- src/Problema3/PeriodoVisible.java
+++ src/Problema3/PeriodoVisible.java
@@ -1,4 +1,20 @@
 package Problema3;
 
 public class PeriodoVisible {
+  private String periodo;
+
+    public PeriodoVisible(String periodo) {
+        this.periodo = periodo;
+    }
+
+    public String getPeriodo() {
+        return periodo;
+    }
+
+    @Override
+    public String toString() {
+        return "PeriodoVisible{" +
+                "periodoObservado='" + periodo + '\'' +
+                '}';
+    }
 }
