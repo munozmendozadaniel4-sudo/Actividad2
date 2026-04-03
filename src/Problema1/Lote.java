d6f68c601ccda460187154ad605311927cde5872
Daniel Muñoz Mendoza <munozmendozadaniel4@gmail.com>
2026-04-02 20:13:49 -0600

    corregido

--- src/Problema1/Lote.java
+++ src/Problema1/Lote.java
@@ -1,4 +1,62 @@
 package Problema1;
+import java.time.LocalDate;
 
 public class Lote {
+    private int numeroLote;
+    private int numeroPiezas;
+    private LocalDate fechaFabricacion;
+    private Prenda prenda;
+
+    public Lote(int numeroLote, int numeroPiezas, LocalDate fechaFabricacion, Prenda prenda) {
+        this.numeroLote = numeroLote;
+        this.numeroPiezas = numeroPiezas;
+        this.fechaFabricacion = fechaFabricacion;
+        this.prenda = prenda;
+    }
+
+    public double calcularCostoProduccion() {
+        return numeroPiezas * prenda.getCostoProduccion();
+    }
+
+    public double calcularMontoRecuperacion() {
+
+        double precioVenta = prenda.calcularPrecioVenta();
+        double precioLote = precioVenta * 1.05;
+
+        return numeroPiezas * precioLote;
+    }
+
+    @Override
+    public String toString() {
+        return "Lote{" +
+                "numeroLote=" + numeroLote +
+                ", numeroPiezas=" + numeroPiezas + " piezas" +
+                ", fechaFabricacion=" + fechaFabricacion +
+                '}';
+    }
+
+    public static void main(String[] args) {
+
+        Prenda prenda = new Prenda(
+                "Camisa deportiva",
+                "Algodon",
+                120,
+                "Masculino",
+                "Verano"
+        );
+
+        Lote lote = new Lote(
+                1,
+                500,
+                LocalDate.now(),
+                prenda
+        );
+
+        System.out.println(prenda);
+        System.out.println(lote);
+
+        System.out.printf("Costo total de produccion del lote: $%.2f\n", lote.calcularCostoProduccion());
+        System.out.printf("Monto estimado de recuperacion del lote: $%.2f\n", lote.calcularMontoRecuperacion());
+    }
 }
+
