69135703bfcadfa1e817f00854df0f15478853a6
Daniel Muñoz Mendoza <munozmendozadaniel4@gmail.com>
2026-04-02 20:14:40 -0600

    corregido

--- src/Problema1/Prenda.java
+++ src/Problema1/Prenda.java
@@ -1,4 +1,53 @@
 package Problema1;
+import java.time.LocalDate;
 
 public class Prenda {
+   private String modelo;
+        private String tela;
+        private double costoProduccion;
+        private String genero;
+        private String temporada;
+
+        public Prenda(String modelo, String tela, double costoProduccion, String genero, String temporada) {
+            this.modelo = modelo;
+            this.tela = tela;
+            this.costoProduccion = costoProduccion;
+            this.genero = genero;
+            this.temporada = temporada;
+        }
+
+        public String getModelo() {
+            return modelo;
+        }
+
+        public String getTela() {
+            return tela;
+        }
+
+        public double getCostoProduccion() {
+            return costoProduccion;
+        }
+
+        public String getGenero() {
+            return genero;
+        }
+
+        public String getTemporada() {
+            return temporada;
+        }
+
+        public double calcularPrecioVenta() {
+            return costoProduccion * 1.15;
+        }
+
+        @Override
+        public String toString() {
+            return "Prenda{" +
+                    "modelo='" + modelo + '\'' +
+                    ", tela='" + tela + '\'' +
+                    ", costoProduccionPorPieza=$" + costoProduccion +
+                    ", genero='" + genero + '\'' +
+                    ", temporada='" + temporada + '\'' +
+                    '}';
+        }
 }
