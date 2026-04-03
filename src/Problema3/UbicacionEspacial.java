c5909195ccc36cef15a438b7122e6d90958b1735
Daniel Muñoz Mendoza <munozmendozadaniel4@gmail.com>
2026-04-02 20:17:29 -0600

    corregido

--- src/Problema3/UbicacionEspacial.java
+++ src/Problema3/UbicacionEspacial.java
@@ -1,4 +1,31 @@
 package Problema3;
 
 public class UbicacionEspacial {
+    private double latitud;
+    private String hemisferioLatitud;
+    private double longitud;
+    private String hemisferioLongitud;
+
+    public UbicacionEspacial(double latitud, String hemisferioLatitud, double longitud, String hemisferioLongitud) {
+        this.latitud = latitud;
+        this.hemisferioLatitud = hemisferioLatitud;
+        this.longitud = longitud;
+        this.hemisferioLongitud = hemisferioLongitud;
+    }
+
+    public double getLatitud() {
+        return latitud;
+    }
+
+    public double getLongitud() {
+        return longitud;
+    }
+
+    @Override
+    public String toString() {
+        return "UbicacionEspacial{" +
+                "latitud=" + latitud + "° " + hemisferioLatitud +
+                ", longitud=" + longitud + "° " + hemisferioLongitud +
+                '}';
+    }
 }
